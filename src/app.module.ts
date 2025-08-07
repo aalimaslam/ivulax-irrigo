4a6b456edb6159dca2684f34471109c672fc8861
Umar farooq <103427953+umarfarooq1901@users.noreply.github.com>
2025-08-07 14:13:49 +0530

    changes app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,26 @@ import { CoreModule } from './core/core.module';
 import { InfraModule } from './infra/infra.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config'; 
+
+// @Module({
+//   imports: [CoreModule, InfraModule, AuthModule, UsersModule],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+
 
 @Module({
-  imports: [CoreModule, InfraModule, AuthModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    CoreModule, 
+    InfraModule, 
+    AuthModule, 
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
